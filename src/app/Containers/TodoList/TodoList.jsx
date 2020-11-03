c822c44bab4e1866abbccabe010a49a1897d9c8c
Ivan <ivan.chuvilko@gmail.com>
2020-11-03 15:18:32 +0300

    Update task

--- src/app/Containers/TodoList/TodoList.jsx
+++ src/app/Containers/TodoList/TodoList.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Button from 'react-bootstrap/Button';
 import {connect} from 'react-redux';
 import TodoItem from '../../Components/TodoItem/TodoItem';
-import {actions}  from './todoSlice';
+import {actions, initialState, todoSlice} from './todoSlice';
+import PropTypes from 'prop-types'
+import { createSelector } from '@reduxjs/toolkit'
 /**
  * todo implement component called ToDoInput
  * which should receive onSubmit function which will be called on the press enter key
@@ -9,7 +12,7 @@ import {actions}  from './todoSlice';
  * this input changes should be managed by local state inside ToDoInput component
  * Use this component for enter tasks name
  */
-//import ToDoInput from "../../Components/TodoInput/ToDoInput";
+import ToDoInput from "../../Components/TodoInput/ToDoInput";
 import {bindActionCreators} from "../../utils/store";
 
 import './TodoList.scss'
@@ -20,29 +23,114 @@ import RadioBadge from "../../Components/RadioBadge/RaidoBadge";
  */
 
 import { controlBadges } from '../../constants/todo';
-
+import {text} from "@fortawesome/fontawesome-svg-core";
 
 /**
  * todo implement HOC for display the list of the todos and control panel and input for add new todos
  */
-class TodoList extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {   //todo setup local state
+
+const TodoList = (props) => {
+    const FILTER_MAP = {
+        All: () => true,
+        ToDo: todo => !todo.completed,
+        Completed: todo => todo.completed
+    };
+
+    const {todos, remove, markAsChecked, clearCompleted, checkAll} = props
+    const [state, setState] = useState({items: todos, filter: 'All'})
+
+    useEffect(()  => {
+        const todoList = todos.filter(FILTER_MAP['All'])
+        setState({items: todoList, filter: 'All'})
+        console.log('LOOG fsdfsdfsdfs');
+    },[todos])
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+    const btnClick = name => () => {
+        const todoList = todos.filter(FILTER_MAP[name])
+        setState({items: todoList, filter: name})
+    };
+
+    const checkAllTodos = () => {
+        console.log(todos)
     }
-  }
-  render() {
-
-    return <div className="container">
-    </div>
-  }
-
+    return (
+        <React.Fragment>
+            <div className="todo-list">
+                <ToDoInput/>
+                <hr/>
+                <div className="list">
+                    {state.items.map((todo, index) => (
+                        <TodoItem
+                        id={todo.id}
+                        index={index}
+                        key={todo.id}
+                        text={todo.text}
+                        onRemove={() => {
+                        remove({id: todo.id, text: todo.text})
+                    }}
+                        markAsChecked={() => {
+                        markAsChecked({id: todo.id, completed: todo.completed})
+                    }}
+                        todo={todo}
+                        />
+                        ))}
+                </div>
+                <div className="footerSection">
+                    <ul className="footer">
+                        <li
+                            className="taskCount"
+                            onClick={checkAll}
+                        >
+                            {todos.length} tasks left
+                        </li>
+                        <li>
+                            {controlBadges.map((name) => (
+                                <Button
+                                    key={name}
+                                    onClick={btnClick(name)}
+                                    name={name}
+                                    className="filterButton"
+                                >
+                                    {name}
+                                </Button>))}
+                        </li>
+                        <li
+                            className="clearTasksButton"
+                            onClick={clearCompleted}
+                        >
+                            Clear completed
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </React.Fragment>
+    )
 }
 
-const mapStateToProps = state => ({});  //todo setup this method for get info from the global state
+const selectTodos = state => state.todos
 
-// const mapDispatchToProps = dispatch => ;  //todo implement this function
+const selectVisibleTodos = createSelector(
+    [selectTodos]
+)
+
+TodoList.propTypes = {
+    todos: PropTypes.array.isRequired,
+}
+
+const mapStateToProps = state => ({todos: state.todo})
+const mapDispatchToProps = {
+    addTodo: actions.addTodo,
+    remove: actions.remove,
+    markAsChecked: actions.markAsChecked,
+    clearCompleted: actions.clearCompleted,
+    checkAll: actions.checkAll
+}
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
+// export default TodoList
--- src/app/Containers/TodoList/todoSlice.js
+++ src/app/Containers/TodoList/todoSlice.js
@@ -3,20 +3,35 @@ import { createSlice } from '@reduxjs/toolkit';
 export const initialState = {
   tasks: [],  // task should have a format {id: unique_value, text: taks_text, checked: flag_show_if_task_completed (false by default) }
 };
+let nextTodoId = 0
 
 export const todoSlice = createSlice({
   name: 'todo',
-  initialState,
+  initialState:[],
   reducers: {
-    add: (state, {payload}) => {  // todo implement function for add new todo into list
+    addTodo: {
+      reducer(state, action) {
+        const { id, text } = action.payload
+
+        state.push({ id, text, completed: false })
+      },
+      prepare(text) {
+        return { payload: { text, id: nextTodoId++ } }
+      }
     },
-    remove: (state, {payload}) => {  // todo implement function for remove todo from the list
+    remove: (state, action, index) => {
+      const { id, text } = action.payload
+      state.splice(state.findIndex(i => i.id === id), 1)
     },
-    markAsChecked: (state, {payload}) => {  // todo implement function for mark task checked by id
+    markAsChecked: (state, action) => {
+      return state.map (todo => todo.id === action.payload.id ? {...todo, completed: !todo.completed} : todo)
     },
-    clearCompleted: state => {  //todo implement funciton for remove all completed (checked ) tasks
+    clearCompleted: (state) => {
+      return state.filter(todo => !todo.completed === true)
     },
-    checkAll: state => {
+    checkAll: (state) => {
+      console.log(state.map(todo => !todo.completed === true))
+      return state.map(todo => todo ? {...todo, completed: !todo.completed} : todo)
     }
   },
 });
